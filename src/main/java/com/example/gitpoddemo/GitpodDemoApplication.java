f83c6ee64fec76f6fcaf31eec082a302cd78ca46
unknown <pavol.valiska@gmail.com>
2019-12-04 21:29:14 +0100

    Initial commit

---
+++ src/main/java/com/example/gitpoddemo/GitpodDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitpoddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpodDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpodDemoApplication.class, args);
+	}
+
+}
